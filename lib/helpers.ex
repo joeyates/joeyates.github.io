600b0adb43dd11fa902d5d7806db8051f3b05f3b
Joe Yates <joe.g.yates@gmail.com>
2023-01-22 12:09:54 +0100

    Extract module

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -3,116 +3,11 @@ defmodule Helpers do
 
   defmacro __using__(_opts \\ %{}) do
     quote do
-      import Fermo.DatoCMS.GraphQLClient, only: [
-        query!: 1, query!: 2, query_for_path!: 3
-      ]
       import DatoCMS.GraphQLClient.MetaTagHelpers
       import FermoHelpers.Links
-
-      @posts_per_page 20
+      import JoeyatesBlog.CMS
 
       def environment, do: System.get_env("BUILD_ENV")
-
-      def home(for_path) do
-        result = query_for_path!(
-          for_path,
-          """
-          query MyQuery {
-            home {
-              title
-              abstract
-            }
-          }
-          """,
-          %{}
-        )
-
-        result[:home]
-      end
-
-      def post_count() do
-        result = query!(
-          """
-          query {
-            _allPostsMeta { count }
-          }
-          """
-        )
-
-        result._allPostsMeta.count
-      end
-
-      def posts_per_page, do: @posts_per_page
-
-      def posts_page(for_path, opts \\ []) do
-        per_page = Keyword.get(opts, :per_page, @posts_per_page)
-        page = Keyword.get(opts, :page, 1)
-        skip = (page - 1) * per_page
-        result = query_for_path!(
-          for_path,
-          """
-          query($first: IntType, $skip: IntType)  {
-            allPosts(orderBy: _createdAt_DESC, skip: $skip, first: $first) {
-              id
-              _createdAt
-              slug
-              title
-              oldPath
-            }
-          }
-          """,
-          %{skip: skip, first: per_page}
-        )
-
-        result[:allPosts]
-      end
-
-      def by_creation_date(collection) do
-        collection
-        |> Enum.sort_by(&(&1._createdAt))
-        |> Enum.reverse()
-      end
-
-      def latest_posts(for_page, opts \\ []) do
-        count = Keyword.get(opts, :count, 10)
-
-        posts_page(for_page)
-        |> by_creation_date()
-        |> Enum.take(count)
-      end
-
-      def post(id, for_path \\ nil) do
-        result = query_for_path!(
-          for_path,
-          """
-          query Post($id: ItemId) {
-            post(filter: {id: {eq: $id}}) {
-              id
-              _createdAt
-              title
-              body {
-                blocks {
-                  __typename
-                  id
-                  image {
-                    url
-                  }
-                  caption
-                }
-                value
-              }
-              categories {
-                name
-                slug
-              }
-            }
-          }
-          """,
-          %{id: id}
-        )
-
-        result[:post]
-      end
 
       @blank_dast %{
         value: %{
---
+++ lib/joeyates_blog/cms.ex
@@ -0,0 +1,108 @@
+defmodule JoeyatesBlog.CMS do
+  @moduledoc false
+
+  alias Fermo.DatoCMS.GraphQLClient
+
+  @posts_per_page 20
+
+  def home(for_path) do
+    result = GraphQLClient.query_for_path!(
+      for_path,
+      """
+      query MyQuery {
+        home {
+          title
+          abstract
+        }
+      }
+      """,
+      %{}
+    )
+
+    result[:home]
+  end
+
+  def post_count() do
+    result = GraphQLClient.query!(
+      """
+      query {
+        _allPostsMeta { count }
+      }
+      """
+    )
+
+    result._allPostsMeta.count
+  end
+
+  def post(id, for_path \\ nil) do
+    result = GraphQLClient.query_for_path!(
+      for_path,
+      """
+      query Post($id: ItemId) {
+        post(filter: {id: {eq: $id}}) {
+          id
+          _createdAt
+          title
+          body {
+            blocks {
+              __typename
+              id
+              image {
+                url
+              }
+              caption
+            }
+            value
+          }
+          categories {
+            name
+            slug
+          }
+        }
+      }
+      """,
+      %{id: id}
+    )
+
+    result[:post]
+  end
+
+  def posts_per_page, do: @posts_per_page
+
+  def posts_page(for_path, opts \\ []) do
+    per_page = Keyword.get(opts, :per_page, @posts_per_page)
+    page = Keyword.get(opts, :page, 1)
+    skip = (page - 1) * per_page
+    result = GraphQLClient.query_for_path!(
+      for_path,
+      """
+      query($first: IntType, $skip: IntType)  {
+        allPosts(orderBy: _createdAt_DESC, skip: $skip, first: $first) {
+          id
+          _createdAt
+          slug
+          title
+          oldPath
+        }
+      }
+      """,
+      %{skip: skip, first: per_page}
+    )
+
+    result[:allPosts]
+  end
+
+  def latest_posts(for_page, opts \\ []) do
+    count = Keyword.get(opts, :count, 10)
+
+    posts_page(for_page)
+    |> by_creation_date()
+    |> Enum.take(count)
+  end
+
+  def by_creation_date(collection) do
+    collection
+    |> Enum.sort_by(&(&1._createdAt))
+    |> Enum.reverse()
+  end
+end
