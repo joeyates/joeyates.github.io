1b72f93e91ecd12d837f896c76796641475b3704
Joe Yates <joe.g.yates@gmail.com>
2023-01-22 12:50:54 +0100

    Extract module

--- lib/joeyates_blog/cms.ex
+++ lib/joeyates_blog/cms.ex
@@ -3,8 +3,6 @@ defmodule JoeyatesBlog.CMS do
 
   alias Fermo.DatoCMS.GraphQLClient
 
-  @posts_per_page 20
-
   def setup do
     DatoCMS.GraphQLClient.configure()
   end
@@ -23,90 +21,18 @@ defmodule JoeyatesBlog.CMS do
       %{}
     )
 
-    result[:home]
-  end
-
-  def post_count() do
-    result = GraphQLClient.query!(
-      """
-      query {
-        _allPostsMeta { count }
-      }
-      """
-    )
-
-    result._allPostsMeta.count
-  end
-
-  def post(id, for_path \\ nil) do
-    result = GraphQLClient.query_for_path!(
-      for_path,
-      """
-      query Post($id: ItemId) {
-        post(filter: {id: {eq: $id}}) {
-          id
-          _createdAt
-          title
-          body {
-            blocks {
-              __typename
-              id
-              image {
-                url
-              }
-              caption
-            }
-            value
-          }
-          categories {
-            name
-            slug
-          }
-        }
-      }
-      """,
-      %{id: id}
-    )
-
-    result[:post]
-  end
-
-  def posts_per_page, do: @posts_per_page
-
-  def posts_page(for_path, opts \\ []) do
-    per_page = Keyword.get(opts, :per_page, @posts_per_page)
-    page = Keyword.get(opts, :page, 1)
-    skip = (page - 1) * per_page
-    result = GraphQLClient.query_for_path!(
-      for_path,
-      """
-      query($first: IntType, $skip: IntType)  {
-        allPosts(orderBy: _createdAt_DESC, skip: $skip, first: $first) {
-          id
-          _createdAt
-          slug
-          title
-          oldPath
-        }
-      }
-      """,
-      %{skip: skip, first: per_page}
-    )
-
-    result[:allPosts]
-  end
-
-  def latest_posts(for_page, opts \\ []) do
-    count = Keyword.get(opts, :count, 10)
-
-    posts_page(for_page)
-    |> by_creation_date()
-    |> Enum.take(count)
+    result.home
   end
 
   def by_creation_date(collection) do
     collection
-    |> Enum.sort_by(&(&1._createdAt))
+    |> Enum.sort(
+      fn x, y ->
+        case DateTime.compare(y._createdAt, x._createdAt) do
+          :gt -> true
+          :lt -> false
+        end
+      end)
     |> Enum.reverse()
   end
 end
