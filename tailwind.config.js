86fd32ef759eb8d867cf2b1c9824894d9e7cfce5
Joe Yates <joe.g.yates@gmail.com>
2023-06-23 12:09:34 +0200

    Scan SLIM templates for Tailwind usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,7 @@ let plugin = require('tailwindcss/plugin')
 
 module.exports = {
   content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
+    './priv/source/**/*.html.slim'
   ],
   theme: {
     extend: {
