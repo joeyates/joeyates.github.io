95bddc30df57990770c63ac8ff9e45df9592d92e
Joe Yates <joe.g.yates@gmail.com>
2023-06-23 17:46:30 +0200

    Style images

--- lib/joeyates_blog/rendering/image_with_caption.ex
+++ lib/joeyates_blog/rendering/image_with_caption.ex
@@ -2,9 +2,9 @@ defmodule JoeyatesBlog.Rendering.ImageWithCaption do
   def render(block, _dat, _options) do
     [
       ~s(
-        <div class="image_with_caption">
-          <img src="#{block.image.url}?w=600"/>
-          <p class="caption">#{block.caption}</p>
+        <div class="w-full my-4 flex flex-column justify-center">
+          <img src="#{block.image.url}?w=600" class="mx-4"/>
+          <p class="flex justify-center text-sm">#{block.caption}</p>
         </div>
       )
     ]
