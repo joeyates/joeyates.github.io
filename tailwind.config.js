07d72b23567f190833ffc4628f0a540e3bc5ac4c
Joe Yates <joe.g.yates@gmail.com>
2023-08-04 10:12:17 +0200

    Use const where possibile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-let plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   content: [
