08561bf42f9c8212012dfe56806b656cd3107757
Joe Yates <joe.g.yates@gmail.com>
2023-08-04 09:55:47 +0200

    Typo

--- lib/joeyates_blog/rendering/image_with_caption.ex
+++ lib/joeyates_blog/rendering/image_with_caption.ex
@@ -2,8 +2,8 @@ defmodule JoeyatesBlog.Rendering.ImageWithCaption do
   def render(block, _dat, _options) do
     [
       ~s(
-        <div class="w-full my-4 flex flex-column justify-center">
-          <img src="#{block.image.url}?w=600" class="mx-4"/>
+        <div class="w-full my-4 flex flex-col justify-center">
+          <img src="#{block.image.url}?w=600"/>
           <p class="flex justify-center text-sm">#{block.caption}</p>
         </div>
       )
