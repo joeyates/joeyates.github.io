09280ec2e0a9e365bcc114fb6ed21e67372c6502
Joe Yates <joe.g.yates@gmail.com>
2025-07-19 15:31:51 +0200

    Add PayloadCMS configuration

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,3 +1,19 @@
 import Config
 
+payloadcms_api_key =
+  System.get_env("PAYLOADCMS_API_KEY") ||
+    raise """
+    environment variable PAYLOADCMS_API_KEY is missing.
+    """
+
+payloadcms_graphql_endpoint =
+  System.get_env("PAYLOADCMS_GRAPHQL_ENDPOINT") ||
+    raise """
+    environment variable PAYLOADCMS_GRAPHQL_ENDPOINT is missing.
+    """
+
+config :payloadcms_graphql_client,
+  api_key: payloadcms_api_key,
+  endpoint: payloadcms_graphql_endpoint
+
 config :fermo, :base_url, System.fetch_env!("BASE_URL")
